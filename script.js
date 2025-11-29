4b8bacfee1c90143adc4411d73026011a66c1117
Kolibrika666 <kolibrika666@gmail.com>
2025-11-29 11:29:36 +0700

    js array colors

---
+++ script.js
@@ -0,0 +1,8 @@
+const colors = []; // массив цветов
+colors[0] = document.getElementById("red");
+colors[1] = document.getElementById("yellow");
+colors[2] = document.getElementById("green");
+colors[3] = document.getElementById("cyan");
+colors[4] = document.getElementById("blue");
+colors[5] = document.getElementById("magenta");
+
